c27155f35892377981f0cecddedda41de42c735c
B2014879-NguyenHuuThien <thienb2014879@student.ctu.edu.vn>
2023-10-19 22:13:03 +0700

    done all edit and add

--- view/edit_users.php
+++ view/edit_users.php
@@ -2,55 +2,82 @@
 require '../include/connect.php';
 require '../include/user_session.php';
 
-$currentUsername = $_SESSION['username'];
+function isEmailUsed($pdo, $email, $username)
+{
+    $checkEmailQuery = "SELECT email FROM users WHERE email = :email AND username != :username";
+    $checkEmailStmt = $pdo->prepare($checkEmailQuery);
+    $checkEmailStmt->bindParam(':email', $email);
+    $checkEmailStmt->bindParam(':username', $username);
+    $checkEmailStmt->execute();
+
+    return $checkEmailStmt->rowCount() > 0;
+}
+
+function isUsernameUsed($pdo, $username, $currentUsername)
+{
+    $checkUsernameQuery = "SELECT username FROM users WHERE username = :username AND username != :currentUsername";
+    $checkUsernameStmt = $pdo->prepare($checkUsernameQuery);
+    $checkUsernameStmt->bindParam(':username', $username);
+    $checkUsernameStmt->bindParam(':currentUsername', $currentUsername);
+    $checkUsernameStmt->execute();
+
+    return $checkUsernameStmt->rowCount() > 0;
+}
+
+$username = $_GET['username'];
+$name_error = "";
+$email_error = "";
+$password_error = "";
 
 if (isset($_POST['submit'])) {
-    $newUsername = $_POST['newUsername'];
-    $newEmail = $_POST['newEmail'];
-    $newPassword = $_POST['newPassword'];
+    $newUsername = $_POST['username'];
+    $email = $_POST['email'];
+    $newPassword = $_POST['password'];
     $role = $_POST['userRole'];
 
-    if (!empty($newUsername) && !empty($newEmail)) {
-        $sql = "UPDATE users SET username = :newUsername, email = :newEmail, access = :access";
+    try {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        if (!empty($newPassword)) {
-            $sql .= ", password = :newPassword";
-        }
+        if (isEmailUsed($pdo, $email, $username)) {
+            $email_error = "<span style='color: red;'>Email đã được sử dụng! Vui lòng sử dụng Email khác.</span>";
+        } elseif (isUsernameUsed($pdo, $newUsername, $username)) {
+            $name_error = "<span style='color: red;'>Tên người dùng đã được sử dụng! Vui lòng sử dụng tên khác.</span>";
+        } else {
+            $updateQuery = "UPDATE users SET username = :newUsername, email = :email, access = :access WHERE username = :currentUsername";
+            $updateStmt = $pdo->prepare($updateQuery);
+            $updateStmt->bindParam(':newUsername', $newUsername);
+            $updateStmt->bindParam(':email', $email);
+            $updateStmt->bindParam(':access', $role);
+            $updateStmt->bindParam(':currentUsername', $username);
 
-        if ($newPassword) {
-            $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
-        }
+            if ($updateStmt->execute()) {
+                // Check if a new password is provided
+                if (!empty($newPassword)) {
+                    $passwordHash = password_hash($newPassword, PASSWORD_DEFAULT);
+                    $passwordUpdateQuery = "UPDATE users SET password = :password WHERE username = :currentUsername";
+                    $passwordUpdateStmt = $pdo->prepare($passwordUpdateQuery);
+                    $passwordUpdateStmt->bindParam(':password', $passwordHash);
+                    $passwordUpdateStmt->bindParam(':currentUsername', $username);
+                    $passwordUpdateStmt->execute();
+                }
 
-        $sql .= " WHERE email = :email AND username = :currentUsername";
-
-        if ($stmt = $pdo->prepare($sql)) {
-            $stmt->bindParam(":newUsername", $newUsername, PDO::PARAM_STR);
-            $stmt->bindParam(":newEmail", $newEmail, PDO::PARAM_STR);
-
-            if (!empty($newPassword)) {
-                $stmt->bindParam(":newPassword", $hashedPassword, PDO::PARAM_STR);
-            }
-
-            $stmt->bindParam(":access", $role, PDO::PARAM_INT);
-            $stmt->bindParam(":email", $email, PDO::PARAM_STR);
-            $stmt->bindParam(":currentUsername", $currentUsername, PDO::PARAM_STR);
-
-            if ($stmt->execute()) {
-                $_SESSION['username'] = $newUsername;
-                $_SESSION['email'] = $newEmail;
-                echo '<script>alert("Cập nhật thông tin thành công");</script>';
-                echo '<script>setTimeout(function(){ window.location.href = "users_manage.php"; }, 100);</script>';
-                exit();
+                echo "<script>alert('Cập nhật người dùng thành công!'); setTimeout(function(){window.location.href='users_manage.php';}, 1);</script>";
             } else {
-                echo "<script>alert('Lỗi: Không thể cập nhật thông tin người dùng.');</script>";
+                echo "<script>alert('Cập nhật người dùng thất bại!');</script>";
             }
         }
-    } else {
-        echo "<script>alert('Vui lòng cung cấp tên người dùng và email hợp lệ.');</script>";
+    } catch (PDOException $e) {
+        echo "<script>alert('Error: " . $e->getMessage() . "');</script>";
     }
 }
-?>
 
+$selectQuery = "SELECT * FROM users WHERE username = :username";
+$selectStmt = $pdo->prepare($selectQuery);
+$selectStmt->bindParam(':username', $username);
+$selectStmt->execute();
+$userData = $selectStmt->fetch();
+
+?>
 <?php include '../include/header.html'; ?>
 <title>Chỉnh sửa người dùng</title>
 <!-- Nav 1 -->
@@ -59,15 +86,12 @@ if (isset($_POST['submit'])) {
         <a class="navbar-brand p-1" href="#">
             <img id="logo" src="../asset/icon/icon.png" alt="Logo">
         </a>
-
         <div class="d-flex justify-content-between">
             <div class="dropdown pt-3">
                 <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
                     id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                     <?php
-                    if (isset($_SESSION['username'])) {
-                        echo "<span style='color: black;'>Xin chào, " . $_SESSION['username'] . "!</span>__";
-                    }
+                    echo "<span style='color: black;'>Xin chào, " . $user['username'] . "!</span>__";
                     ?>
 
                     <img src="../asset/icon/profile-user.png" alt="user.png" width="35" height="35"
@@ -85,95 +109,87 @@ if (isset($_POST['submit'])) {
     </div>
 </nav>
 <!-- End Nav 1 -->
-
-<div class="container">
+<div class="container mb-5">
     <div class="form-container">
-        <form name="form_account" id="form_account" action="edit_users.php" method="POST">
+        <form name="form_edit_user" id="form_edit_user" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . '?username=' . $username); ?>" method="POST">
             <div class="title-image mb-3">
                 <img src="../asset/icon/edit_user.png" alt="Hình ảnh tiêu đề">
             </div>
             <div class="mb-3">
-                <label for="newUsername" class="form-label"><i class="fa-solid fa-user"></i> Tên người dùng
-                    mới:</label>
-                <input type="text" class="form-control" id="newUsername" name="newUsername"
-                    value="<?php echo $user['username'] ?>" required>
+                <label for="username" class="form-label"><i class="fa-solid fa-user"></i> Tên người dùng:</label>
+                <input type="text" class="form-control" id="username" name="username" value="<?php echo $userData['username']; ?>" required>
+                <span class="error">
+                    <?php echo $name_error; ?>
+                </span>
             </div>
             <div class="mb-3">
-                <label for="newEmail" class="form-label"><i class="fa-solid fa-envelope"></i> Email mới:</label>
-                <input type="text" class="form-control" id="newEmail" name="newEmail"
-                    value="<?php echo $user['email'] ?>" required>
+                <label for="email" class="form-label"><i class="fa-solid fa-envelope"></i> Email:</label>
+                <input type="email" class="form-control" id="email" name="email" value="<?php echo $userData['email']; ?>" required>
+                <span class="error">
+                    <?php echo $email_error; ?>
+                </span>
             </div>
             <div class="mb-3">
-                <label for="newPassword" class="form-label"><i class="fa-solid fa-key"></i> Mật khẩu mới:</label>
-                <input type="password" class="form-control" id="newPassword" name="newPassword"
-                    placeholder="Nhập mật khẩu mới">
+                <label for="password" class="form-label"><i class="fa-solid fa-key"></i> Mật khẩu:</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Để trống nếu không muốn thay đổi">
+                <span class="error">
+                    <?php echo $password_error; ?>
+                </span>
             </div>
             <div class="mb-3">
                 <label for="userRole" class="form-label"><i class="fa-solid fa-users"></i> Vai trò:</label>
                 <select class="form-control" id="userRole" name="userRole" required>
-                    <option value="0" <?php echo ($user['access'] == 0) ? 'selected' : ''; ?>>Admin</option>
-                    <option value="1" <?php echo ($user['access'] == 1) ? 'selected' : ''; ?>>User</option>
+                    <option value="0" <?php if ($userData['access'] == 0) echo "selected"; ?>>Admin</option>
+                    <option value="1" <?php if ($userData['access'] == 1) echo "selected"; ?>>User</option>
                 </select>
             </div>
             <div class="mb-3 center-button">
-                <button type="submit" name="submit" class="btn btn-success">Cập nhật</button>
+                <button type="submit" name="submit" class="btn btn-success">Cập nhật người dùng</button>
             </div>
         </form>
     </div>
 </div>
 <script>
-    $.validator.addMethod(
-        "customPassword",
-        function (value, element) {
-            return /^(?=.*[A-Za-z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).*$/.test(value);
-        },
-        "Mật khẩu phải chứa ít nhất 1 chữ cái và 1 ký tự đặc biệt"
-    );
-
-    $("#form_account").validate({
-        rules: {
-            newUsername: {
-                required: true,
-                minlength: 2,
+    $(document).ready(function () {
+        $("#form_edit_user").validate({
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 2,
+                },
+                email: {
+                    required: true,
+                    email: true,
+                },
+                password: {
+                    minlength: 6,
+                },
             },
-            newEmail: {
-                required: true,
-                email: true,
+            messages: {
+                username: {
+                    required: 'Bạn chưa nhập tên người dùng',
+                    minlength: 'Tên người dùng phải có ít nhất 2 ký tự'
+                },
+                email: 'Địa chỉ Email không hợp lệ',
+                password: 'Mật khẩu phải có ít nhất 6 ký tự'
             },
-            newPassword: {
-                required: true,
-                minlength: 6,
-                customPassword: true,
+            errorElement: 'div',
+            errorPlacement: function (error, element) {
+                error.addClass('invalid-feedback');
+                if (element.prop('type') == 'checkbox') {
+                    error.insertAfter(element.siblings('label'));
+                } else {
+                    error.insertAfter(element);
+                }
             },
-        },
-        messages: {
-            newUsername: {
-                required: 'Bạn chưa nhập tên người dùng',
-                minlength: 'Tên người dùng phải có ít nhất 2 ký tự'
+            highlight: function (element, errorClass, validClass) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
             },
-            newEmail: 'Địa chỉ Email không hợp lệ',
-            newPassword: {
-                required: 'Bạn chưa nhập mật khẩu',
-                minlength: 'Mật khẩu phải có ít nhất 6 ký tự'
+            unhighlight: function (element, errorClass, validClass) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
             },
-        },
-        errorElement: 'div',
-        errorPlacement: function (error, element) {
-            error.addClass('invalid-feedback');
-            if (element.prop('type') == 'checkbox') {
-                error.insertAfter(element.siblings('label'));
-            } else {
-                error.insertAfter(element);
-            }
-        },
-        highlight: function (element, errorClass, validClass) {
-            $(element).addClass('is-invalid').removeClass('is-valid');
-        },
-        unhighlight: function (element, errorClass, validClass) {
-            $(element).addClass('is-valid').removeClass('is-invalid');
-        },
-    });  
+        });
+    });
 </script>
 </body>
-
 </html>
--- view/login.php
+++ view/login.php
@@ -36,55 +36,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="vi">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng Nhập</title>
-    <link rel="icon" type="image.png" href="asset/icon/favicon.png">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="../asset/style.css">
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-    <script src="../asset/script.js"></script>
-</head>
-
-<body>
-    <div class="container">
-        <div class="form-container">
-            <form name="form_login" id="form_login" action="login.php" method="POST">
-                <div class="title-image">
-                    <img src="../asset/icon/login.png" alt="Hình ảnh tiêu đề">
-                </div>
-                <div class="mb-3">
-                    <label for="login" class="form-label"><i class="fa-solid fa-envelope"></i> Email hoặc tên đăng nhập</label>
-                    <input type="text" class="form-control" id="login" name="login" placeholder="Email hoặc tên đăng nhập" required>
-                    <?php
-                    if (!empty($login_error)) {
-                        echo '<div class="alert alert-danger mt-1">' . $login_error . '</div>';
-                    }
-                    ?>
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label"><i class="fa-solid fa-key"></i> Mật khẩu</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Mật khẩu" required>
-                    <?php
-                    if (!empty($password_error)) {
-                        echo '<div class="alert alert-danger mt-1">' . $password_error . '</div>';
-                    }
-                    ?>
-                </div>
-                Chưa có tài khoản?<a href="register.php"> Đăng ký</a>
-                <div class="center-button mt-3">
-                    <button type="submit" class="btn btn-success">Đăng nhập</button>
-                </div>
-            </form>
-        </div>
+<?php include '../include/header.html'; ?>
+<title>Đăng nhập</title>
+<div class="container">
+    <div class="form-container">
+        <!-- Form -->
+        <form name="form_login" id="form_login" action="login.php" method="POST">
+            <div class="title-image">
+                <img src="../asset/icon/login.png" alt="Hình ảnh tiêu đề">
+            </div>
+            <div class="mb-3">
+                <label for="login" class="form-label"><i class="fa-solid fa-envelope"></i> Email hoặc tên đăng
+                    nhập</label>
+                <input type="text" class="form-control" id="login" name="login" placeholder="Email hoặc tên đăng nhập"
+                    required>
+                <?php
+                if (!empty($login_error)) {
+                    echo '<div class="alert alert-danger mt-1">' . $login_error . '</div>';
+                }
+                ?>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label"><i class="fa-solid fa-key"></i> Mật khẩu</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Mật khẩu"
+                    required>
+                <?php
+                if (!empty($password_error)) {
+                    echo '<div class="alert alert-danger mt-1">' . $password_error . '</div>';
+                }
+                ?>
+            </div>
+            Chưa có tài khoản?<a href="register.php"> Đăng ký</a>
+            <div class="center-button mt-3">
+                <button type="submit" class="btn btn-success">Đăng nhập</button>
+            </div>
+        </form>
+        <!-- End Form -->
     </div>
+</div>
 </body>
 
 </html>
