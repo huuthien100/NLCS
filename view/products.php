8a00042fb5e373e31682903cc1b2547277c0f98f
B2014879-NguyenHuuThien <thienb2014879@student.ctu.edu.vn>
2023-10-25 03:14:41 +0700

    search all

--- view/products.php
+++ view/products.php
@@ -4,18 +4,33 @@ require '../include/user_session.php';
 
 $categoryName = isset($_GET['category']) ? $_GET['category'] : pathinfo(basename($_SERVER['PHP_SELF']), PATHINFO_FILENAME);
 
+$searchText = isset($_POST['search']) ? $_POST['search'] : '';
+
 try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "SELECT p.product_name, p.product_img, p.product_price, c.name_category,p.id_product
-            FROM products p
-            INNER JOIN category c ON p.id_category = c.id_category
-            WHERE c.name_category = :categoryName";
+    if (!empty($searchText)) {
+        $sql = "SELECT p.product_name, p.product_img, p.product_price, c.name_category, p.id_product
+                FROM products p
+                INNER JOIN category c ON p.id_category = c.id_category
+                WHERE c.name_category = :categoryName AND p.product_name LIKE :searchText";
 
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':categoryName', $categoryName, PDO::PARAM_STR);
-    $stmt->execute();
-    $productInformation = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':categoryName', $categoryName, PDO::PARAM_STR);
+        $stmt->bindValue(':searchText', '%' . $searchText . '%', PDO::PARAM_STR);
+        $stmt->execute();
+        $productInformation = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        $sql = "SELECT p.product_name, p.product_img, p.product_price, c.name_category, p.id_product
+                FROM products p
+                INNER JOIN category c ON p.id_category = c.id_category
+                WHERE c.name_category = :categoryName";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':categoryName', $categoryName, PDO::PARAM_STR);
+        $stmt->execute();
+        $productInformation = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
     $userEmail = $_SESSION['email'];
 
@@ -40,7 +55,21 @@ try {
     header("Location: ../view/error.php");
     exit;
 }
+//Search
+$searchResults = array();
+
+if (isset($_POST['search'])) {
+    $search = $_POST['search'];
+    $search = htmlspecialchars($search);
+
+    $query = "SELECT * FROM products WHERE product_name LIKE :search";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindValue(':search', "%" . $search . "%", PDO::PARAM_STR);
+    $stmt->execute();
+    $searchResults = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="vi">
 
@@ -108,69 +137,108 @@ try {
     <div class="divider"></div>
     <!-- Nav 2 -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary stick-nav">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ms-5">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Gundam
-                        </a>
-                        <ul class="dropdown-menu">
-                            <?php
-                            foreach ($categories as $category) {
-                                $categoryName = strtolower($category['name_category']);
-                                if ($categoryName !== 'tool' && $categoryName !== 'base') {
-                                    echo '<a class="dropdown-item" href="products.php?category=' . $categoryName . '">' . $category['name_category'] . '</a>';
-                                }
-                            }
-                            ?>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="navbar-nav ms-5">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Gundam
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <?php
+                                    foreach ($categories as $category) {
+                                        $categoryName = strtolower($category['name_category']);
+                                        if ($categoryName !== 'tool' && $categoryName !== 'base') {
+                                            echo '<a class="dropdown-item" href="products.php?category=' . $categoryName . '">' . $category['name_category'] . '</a>';
+                                        }
+                                    }
+                                    ?>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="products.php?category=tool">Tool</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="products.php?category=base">Base</a>
+                            </li>
                         </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="products.php?category=tool">Tool</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="products.php?category=base">Base</a>
-                    </li>
-                </ul>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                        <ul class="navbar-nav me-5">
+                            <form class="nav-item d-flex pe-3" role="search" method="POST"
+                                action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                <input class="form-control me-2" type="search" name="search" placeholder="Tìm kiếm"
+                                    aria-label="Search">
+                                <button class="btn btn-dark rounded-2" type="submit"><i
+                                        class="fa-solid fa-magnifying-glass"></i></button>
+                            </form>
+                            <li class="nav-item">
+                                <a href="cart.html" class="nav-icon"><i class="fas fa-shopping-cart "></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="cart.html" class="nav-icon"><i class="fas fa-shopping-cart me-5"></i></a>
-                </li>
-            </ul>
         </div>
     </nav>
     <!-- End Nav 2 -->
-    <!-- Product -->
+    <!-- Product and Search -->
     <div class="container-lg">
-        <div class="m-3 text-center mx-auto">
-            <div class="row">
-                <?php
-                if (isset($productInformation) && is_array($productInformation)) {
-                    foreach ($productInformation as $proinf) {
-                        echo '<div class="col-md-4 mt-3 col-sm-6">
-                        <div class="card">
-                            <img src="' . $proinf["product_img"] . '" class="card-img-top p-3" alt="' . $proinf["product_name"] . '">
-                            <div class="card-body">
-                                <h5 class="card-title">' . $proinf["product_name"] . '</h5>
-                                <p class="card-text">' . number_format($proinf["product_price"], 0, '', '.') . ' VNĐ</p>
-                                <a href="product_detail.php?id_product=' . $proinf["id_product"] . '" class="btn btn-danger">Xem chi tiết</a>
-                                </div>
+    <?php
+    if (isset($_POST['search'])) {
+        $searchText = $_POST['search'];
+        if (strlen($searchText) === 0) {
+            echo '<div class="alert alert-danger text-center mt-3" role="alert">Vui lòng nhập từ khóa để tìm kiếm sản phẩm</div>';
+        } else {
+            if (!empty($searchResults)) {
+                echo '<div class="row ">';
+                foreach ($searchResults as $product) {
+                    echo '<div class="col-lg-4 col-sm-6 col-md-6 mt-3 mb-3">
+                    <div class="card text-center">
+                        <img src="' .$product['product_img']. '"
+                            class="card-img-top p-3" alt="' . $product['product_img'] . '">
+                        <div class="card-body">
+                            <h5 class="card-title">' . $product['product_name'] . '</h5>
+                            <p class="card-text">' . number_format($product['product_price']) . ' VNĐ</p>
+                            <a href="product_detail.php?id_product=' . $product["id_product"] . '" class="btn btn-danger">Xem chi tiết</a>
                         </div>
-                    </div>';
-                    }
+                    </div>
+                </div>';
                 }
-                ?>
-            </div>
-        </div>
+                echo '</div>';
+            } else {
+                echo '<div class="alert alert-danger text-center mt-3" role="alert">Sản phẩm bạn tìm kiếm không tồn tại</div>';
+            }
+        }
+    } else {
+        echo '<div class="row ">';
+        if (isset($productInformation) && is_array($productInformation)) {
+            foreach ($productInformation as $proinf) {
+                echo '<div class="col-md-4 mt-3 col-sm-6 text-center">
+                <div class="card">
+                    <img src="' . $proinf["product_img"] . '" class="card-img-top p-3" alt="' . $proinf["product_name"] . '">
+                    <div class="card-body">
+                        <h5 class="card-title">' . $proinf["product_name"] . '</h5>
+                        <p class="card-text">' . number_format($proinf["product_price"], 0, '', '.') . ' VNĐ</p>
+                        <a href="product_detail.php?id_product=' . $proinf["id_product"] . '" class="btn btn-danger">Xem chi tiết</a>
+                    </div>
+                </div>
+            </div>';
+            }
+        }
+        echo '</div>';
+    }
+    ?>
+    <!-- End Product and Search -->
     </div>
-    <!-- End Product -->
+
     <?php include '../include/footer.html'; ?>
 </body>
 
