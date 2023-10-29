dcf3b124db768a84aa3cb508f21e8cfda0dd2bec
B2014879-NguyenHuuThien <thienb2014879@student.ctu.edu.vn>
2023-10-29 17:04:48 +0700

    error

--- view/error.php
+++ view/error.php
@@ -1,14 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Lỗi</title>
-</head>
-<body>
-    <div>
-        <h1>Xảy ra lỗi</h1>
-        <p>Chúng tôi xin lỗi, nhưng có một lỗi xảy ra trong quá trình xử lý yêu cầu của bạn.</p>
-        <p>Vui lòng thử lại sau, hoặc liên hệ với đội hỗ trợ của chúng tôi nếu vấn đề vẫn tồn tại.</p>
+<?php
+require '../include/connect.php';
+require '../include/user_session.php';
+?>
+<?php include '../include/header.php'; ?>
+<title>Lỗi</title>
+    <div class="container">
+        <div class="alert alert-danger mt-5">
+            <h1 class="alert-heading">Xảy ra lỗi</h1>
+            <p>Chúng tôi xin lỗi, nhưng có một lỗi xảy ra trong quá trình xử lý yêu cầu của bạn.</p>
+            <p>Vui lòng thử lại sau, hoặc liên hệ với đội hỗ trợ của chúng tôi nếu vấn đề vẫn tồn tại.</p>
+        </div>
     </div>
+
+    <?php include '../include/footer.html'; ?>
 </body>
+
 </html>
